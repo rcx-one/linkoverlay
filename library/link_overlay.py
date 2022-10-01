06d1b5dd7cef64dc913cf852088070473d30a0e2
Eike Gebauer <e.g@wwu.de>
2022-10-01 04:51:24 +0200

    Second draft, now with custom Tree class, because os.path is clunky and pathlib is incomplete

--- library/link_overlay.py
+++ library/link_overlay.py
@@ -5,86 +5,89 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from ansible.module_utils.basic import AnsibleModule
-from typing import Dict
+from typing import Dict, List, Optional
 import os
+from os import path as osp
+from dataclasses import dataclass
 
 MODULE_ARGS = {
-    "overlay": {
+    "base_dir": {
         "description": [
-            "Directory or file on the managed node to overlay the target.",
-            "Symlinks will point to this directory."
+            "The directory on the managed node where the overlay will be "
+            + "applied.",
+
+            "All symlinks pointing into overlay_dir will be created in this "
+            + "directory."
         ],
         "type": "str",
         "required": True
     },
-    "target": {
-        "description": (
-            "Directory on the managed node where the symlinks will be created."
-        ),
+    "overlay_dir": {
+        "description": [
+            "The directory on the managed node where the overlay files "
+            + "reside.",
+
+            "All created symlinks will point into this directory."
+        ],
         "type": "str",
         "required": True
     },
-    "relative": {
-        "description": "Whether the symlinks will be relative or absolute",
+    "relative_links": {
+        "description": (
+            "Whether relative or absolute symlinks will be created."
+        ),
         "type": "bool",
         "required": False,
         "default": True
     },
     "conflict": {
         "description": [
-            "Behaviour when a file or symlink pointed outside of overlay "
-            + "already exists where a symlink should be created.",
-            "error: Fails on conflicts.",
-            "ignore: Ignores overlay file and keeps the original.",
-            "warning: Like ignore, but prints a warning.",
-            "replace: Replaces original file with symlink to overlay."
+            "How files existing in both the base_dir tree and the overlay_dir "
+            + "tree will be handled:",
+
+            "error: Will fail on conflict.",
+
+            "ignore: Will ignore overlay files and keep the base files.",
+
+            "warning: Like ignore, but will print a warning.",
+
+            "replace: Will replace original file with symlink to overlay.",
+
+            "Symlinks pointing into overlay_dir are always replaced."
         ],
         "type": "str",
         "required": False,
         "default": "error",
         "choices": ["error", "warning", "ignore", "replace"]
     },
-    # "recursive": {
-    #     "description": [
-    #         "If overlay is a directory, recursive decides if a single "
-    #         + "symlink to the directory will be created (False) or "
-    #         + "if symlinks for each file inside overlay and overlay "
-    #         + "subdirectories will be created (True).",
-    #         "If overlay is a file, recursive has no effect and a single "
-    #         + "symlink to the file will be created."
-    #     ],
-    #     "type": "bool",
-    #     "required": False,
-    #     "default": True
-    # },
-    "exclusive": {
-        # TODO: you cant always traverse the whole target directory to find
-        # broken symlinks.
-        # add collapse option and only remove symlinks on the same level as
-        # existing symlinks?
+    "collapse": {
         "description": [
-            "If set to True, broken symlinks from target pointing into "
-            + "overlay will be removed.",
-            "If set to False, broken symlinks into overlay will be kept."
+            "Whether non-conflicting directory trees will be collapsed into a "
+            + "single symlink.",
+
+            "If enabled, will create symlinks to whole subtrees of the "
+            + "overlay_dir if they dont conflict with the base_dir.",
+
+            "If disabled, will only create symlinks to leaves of the "
+            + "overlay_dir tree and creates missing directories in the "
+            + "base_dir."
         ],
         "type": "bool",
         "required": False,
-        "default": True
+        "default": True,
     },
 }
 
 
 def generate_option_doc(
-    option_name: str,
-        option_attributes: Dict,
-        indent: str = " "*4
-):
+    option_name: str, option_attributes: Dict, indent: str = " "*4
+) -> str:
     doc_string = indent + option_name + ":\n"
     for name, val in option_attributes.items():
         doc_string += indent * 2 + str(name) + ": "
 
         if isinstance(val, list):
-            value_string = '\n'.join(str(line) for line in val)
+            value_string = "\n".join(str(line) for line in val)
         elif hasattr(val, "__name__"):
             value_string = val.__name__
         else:
@@ -95,15 +98,16 @@ def generate_option_doc(
             for line in value_string.split("\n")
         ]
         if len(lines) > 1:
-            doc_string += '['
+            doc_string += "["
             doc_string += ", ".join('"' + line + '"' for line in lines)
             doc_string += "]\n"
         else:
-            doc_string += lines[0] + '\n'
+            doc_string += lines[0] + "\n"
+    return doc_string
 
 
 def generate_options_doc():
-    return ''.join(generate_option_doc(name, attrs)
+    return "".join(generate_option_doc(name, attrs)
                    for name, attrs in MODULE_ARGS.items())
 
 
@@ -112,9 +116,9 @@ DOCUMENTATION = (
 ---
 module: link_overlay
 
-short_description: Overlay a directory tree onto a target via symlinks
+short_description: Overlay a directory tree onto a base via symlinks
 
-description: This module creates and manages directories and symlinks to overlay one directory tree onto another.
+description: This module creates and manages symlinks (and directories) to overlay one directory tree onto another.
 
 version_added: "2.3.4"
 
@@ -165,100 +169,109 @@ message:
 '''
 
 
-def points_to(link, path):
-    assert os.path.islink(link)
-    return os.path.realpath(link) == os.path.realpath(path)
+@dataclass
+class Tree():
+    """A class representing a filesystem directory tree.
+    This tree may or may not actually exist on the filesystem.
+    Symlinks are treated like files.
+    """
+    path: str
+    children: List["Tree"]
+    is_dir: bool
+    depth: Optional[int]
 
+    @staticmethod
+    def from_path(path: str, depth: Optional[int] = None) -> "Tree":
+        """Creates a tree recursively from an existing path.
+        Does not recurse into symlinks to directories.
+        """
+        assert depth is None or depth >= 0
+        assert osp.isabs(path)
+        assert osp.exists(path)
 
-def points_into(link, path):
-    assert os.path.islink(link)
-    return (
-        os.path.commonpath(
-            os.path.realpath(link),
-            os.path.realpath(path)
-        ) == path
-    )
+        is_dir = osp.isdir(path) and not osp.islink(path)
 
+        children = []
+        if is_dir and depth != 0:
+            child_depth = None if depth is None else depth - 1
+            children = [
+                Tree.from_path(child, child_depth)
+                for child
+                in map(lambda p: osp.join(path, p), os.listdir(path))
+            ]
 
-def recursive_scandir(path: str):
-    entries = []
-    with os.scandir(os.path.abspath(path)) as it:
-        for entry in it:
-            entries.append(entry)
-            if entry.is_dir(follow_symlinks=True):
-                entries += recursive_scandir(entry.path)
-    return entries
+        elif not osp.isdir(path) and not osp.isfile(path):
+            raise Exception("Path exists, but is neither file nor directory?!")
+
+        return Tree(
+            path=path,
+            children=children,
+            is_dir=is_dir,
+            depth=depth
+        )
+
+    @property
+    def name(self):
+        return osp.basename(self.name)
+
+    @property
+    def files(self):
+        if self.is_dir:
+            files = []
+            for child in self.children:
+                files += child.files
+            return files
+        else:
+            return [self.path]
+
+    def apply(self, func):
+        """Applies a function to this tree and all its children recursively.
+        """
+        func(self)
+        for child in self.children:
+            child.apply(func)
+
+    def map(self, func):
+        """Like apply, but captures the function output in recursive list.
+        """
+        return [func(self), [child.map(func) for child in self.children]]
+
+    def all(self, func):
+        """True, if func returns true for this tree and all of its children
+        """
+        return func(self) and all(child.all(func) for child in self.children)
+
+    def any(self, func):
+        """True, if func returns true for this tree or any of its children
+        """
+        return func(self) or any(child.any(func) for child in self.children)
 
 
 def run_module():
-    result = {"changed": False, "original_message": "", "message": ""}
+    result = {"changed": False, "message": ""}
 
     module = AnsibleModule(
         argument_spec=MODULE_ARGS,
         supports_check_mode=True
     )
 
-    target = os.path.abspath(module.params["target"])
-    overlay = os.path.abspath(module.params["overlay"])
-    # recursive = module.params["recursive"]
+    base_dir = osp.expanduser(module.params["base_dir"])
+    overlay_dir = osp.expanduser(module.params["overlay_dir"])
 
-    if not os.path.exists(target):
-        module.fail_json(msg="Target does not exist", **result)
+    if not osp.isdir(base_dir):
+        module.fail_json(
+            msg="base_dir has to exist and be a directory", **result
+        )
+    if not osp.isdir(overlay_dir):
+        module.fail_json(
+            msg="overlay_dir has to exist and be a directory", **result
+        )
 
-    if not os.path.exists(overlay):
-        module.fail_json(msg="Overlay does not exist", **result)
+    for overlay_path in os.listdir(overlay_dir):
+        base_path = osp.join(base_dir, osp.relpath(overlay_path, overlay_dir))
 
-    if os.path.isfile(overlay):  # or not recursive:
-        overlay_paths = ["."]
-    else:
-        overlay_paths = [
-            os.path.relpath(entry.path, overlay)
-            for entry in recursive_scandir(overlay)
-        ]
-
-    overlay_files = [path for path in overlay_paths if path.is_file()]
-    target_files = [
-        os.path.join(target, overlay_file)
-        for overlay_file in overlay_files
-    ]
-
-    conflicts = []
-    existing = []
-    for overlay_file, target_file in zip(overlay_files, target_files):
-        if not os.path.exists(target_file):
-            continue
-
-        if os.path.islink(target_file):
-            if points_to(target_file, overlay_file):
-                existing.append(overlay_file)
-                continue
-            if points_into(target_file, overlay):
-                continue
-
-        conflicts.append(target_file)
-
-    if len(conflicts) > 0:
-        message = "Conflicts found:\n"
-        for conflict in conflicts:
-            message += conflict + '\n'
-
-        if module.params["conflict"] == "error":
-            module.fail_json(msg=message, **result)
-        elif module.params["conflict"] == "warning":
-            module.warn(message)
-
-    missing = set(overlay)
-
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
     if module.check_mode:
         module.exit_json(**result)
-
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
 
     # use whatever logic you need to determine whether or not this module
     # made any modifications to your target
